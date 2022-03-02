f44f96eb2e596659c413f67bd21355e9b37eb952
oscardelalanza <oscardelalanza@gmail.com>
2022-03-02 14:58:20 -0800

    basic container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  align-items: center;
+`
 function App() {
-  return ()
+  return (
+    <Container>
+      Hello
+    </Container>
+  )
 }
 
 export default App
