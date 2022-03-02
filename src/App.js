1b0bcad23687b5b454d4d4dffd1ff9de5399face
oscardelalanza <oscardelalanza@gmail.com>
2022-03-02 15:52:13 -0800

    card

--- src/App.js
+++ src/App.js
@@ -7,10 +7,18 @@ const Container = styled.div`
   align-items: center;
 `
 
+const Section = styled.section`
+  background-color: #eee;
+  border-top: solid 2px palevioletred;
+  padding: 20px 25px;
+  width: 500px;
+  box-shadow: 0px 2px 3px rgb(0, 0, 0, 0.3);
+`
+
 function App() {
   return (
     <Container>
-      Hello
+      <Section>Hello</Section>
     </Container>
   )
 }
