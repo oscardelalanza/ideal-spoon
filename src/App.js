a7fc9b805cbb41134cc2736123081cffc17e874e
oscardelalanza <oscardelalanza@gmail.com>
2022-03-03 02:03:14 -0800

    formula

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import styled from 'styled-components'
 import { Formik, Form } from 'formik'
 import Input from './components/Input'
 import Button from './components/Button'
 import Container from './components/Container'
 import Section from './components/Section'
 
+const compoundInterest = (deposit, contribution, years, rate) => {
+  let total = deposit
+  for (let i = 0; i < years; i++) {
+    total = (total + contribution) * (rate + 1)
+  }
+
+  return Math.round(total)
+}
+
 function App() {
-  const handleSubmit = () => {}
+  const handleSubmit = ({ deposit, contribution, years, rate}) => {
+    const val = compoundInterest(Number(deposit), Number(contribution), Number(years), Number(rate))
+    console.log(val);
+  }
+
   return (
     <Container>
       <Section>
@@ -17,7 +29,7 @@ function App() {
             years: '',
             rate: ''
           }}
-          onSubmit={handleSubmit()}
+          onSubmit={handleSubmit}
         >
           <Form>
             <Input name="deposit" label="Initial deposit" />
